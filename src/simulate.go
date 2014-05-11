720441d24fd83cf04442a775d199d63cc9ed1c4d
Pavan Yalamanchili <contact@pavanky.com>
2014-05-11 09:14:14 -0400

    Added function to show points table (unsorted)

--- src/simulate.go
+++ src/simulate.go
@@ -4,8 +4,16 @@ import (
     "fmt"
     "os"
     "io"
+    "strconv"
     "encoding/csv"
 )
+
+type Standing struct {
+    name string
+    matches int
+    points float64
+}
+
 
 func readContents(dir string, name string) [][]string {
     file, err := os.Open(dir + "/" + name)
@@ -32,9 +40,41 @@ func readContents(dir string, name string) [][]string {
     return contents
 }
 
-func showContents(contents [][]string) {
-    for _, line := range contents {
-        fmt.Println(line)
+func currentStandings(matches [][]string) map[string]Standing {
+
+    var m map[string]Standing
+    m = make(map[string]Standing)
+
+    for _, match := range matches {
+        for i := 0; i < 2; i++ {
+            team := match[i]
+            pts := match[i + 2]
+
+            if (pts == "X") {
+                break
+            }
+
+            points, err := strconv.ParseFloat(pts,32)
+            if err != nil {
+                panic(err)
+            }
+
+            c := m[team]
+            c.matches = c.matches + 1
+            c.points = c.points + points
+            c.name = team
+            m[team] = c
+        }
+    }
+
+    return m
+}
+
+func showStandings(standings map[string]Standing) {
+
+    fmt.Printf("Team Name\t Matches\t Points\t\n")
+    for key, val := range standings {
+        fmt.Printf("%s\t\t %2d\t\t %.0f\t\n", key, val.matches, val.points)
     }
 }
 
@@ -42,5 +82,6 @@ func main() {
     dir := os.Args[1]
 
     matches := readContents(dir, "matches.csv")
-    showContents(matches)
+    standings := currentStandings(matches[1:])
+    showStandings(standings)
 }
